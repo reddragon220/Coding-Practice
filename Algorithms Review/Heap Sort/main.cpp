59f782cecf7138b52475192e049dda5b456d30a6
Alex <reddragon220@gmail.com>
2011-02-04 15:58:48 -0500

    Corrected Heapsort

--- Algorithms Review/Heap Sort/main.cpp
+++ Algorithms Review/Heap Sort/main.cpp
@@ -4,13 +4,15 @@
 // Structure:
 // every element A[i] has it's left and right child at A[2i] and A[2i+1]
 // parent is i/2
+//
+// The initial call to buildHeap takes O(n) and then the n calls to heapify cost O(logn) each
 #include <iostream>
 #include <vector>
+#define DEBUG
 using namespace std;
 
 void sort(vector <int> &array);
-//void buildHeap(vector <int> &array);
-//void heapify(vector <int> &array, int index, int max);
+void buildHeap(vector <int> &array);
 void heapify(vector <int> &array, int index);
 
 int main (int argc, char * const argv[]) {
@@ -37,20 +39,40 @@ int main (int argc, char * const argv[]) {
     return 0;
 }
 
-
-// start at the last node and call heapify on each node to make sure everything
+// We first build a heap in place in the array, then we begin sorting it.
+// Start at the last node and call heapify on each node to make sure everything
 // is a heap as the loop invariant moves towards the front.
 void sort(vector <int> &array){
-	for (int i = array.size(); i >= 0; i--) {
-			swap(array[0], array[i]);
+	buildHeap(array);
+	
+	#ifdef DEBUG
+	for (int i=0; i < array.size(); i++) {
+		cout << array[i] << " ";
+	}
+	cout << endl;
+	#endif
+	
+	for (int i = array.size()-1; i >= 0; i--) {
+			swap(array[0], array[i]);		
 			heapify(array, i);
 		
-		for (int i=0; i < array.size(); i++) {
-			cout << array[i] << " ";
-		}
-		cout << endl;
+			#ifdef DEBUG
+			for (int i=0; i < array.size(); i++) {
+				cout << array[i] << " ";
+			}
+			cout << endl;
+			#endif DEBUG
+
 	}
 	
+}
+
+// Start at the last node that can possibly have children which could violate the heap property and fix working bottom up until it reaches the root at the top
+// Simply put - heapify each non-leaf node
+void buildHeap(vector <int> &array){
+	for (int i = (array.size())/2; i >= 0; i--){
+		heapify(array, i);
+	}
 }
 
 // Max heapify - take a node and make sure that it's larger than all of its children.
@@ -61,20 +83,15 @@ void heapify(vector <int> &array, int index){
 	
 	int largest = index;
 	
-	if (left <= array.size() && array[left] > array[largest]){
+	if (left <= array.size()-1 && array[left] > array[largest]){
 		largest = left;
 	}
-	if (right <= array.size() && array[right] > array[largest]){
+	if (right <= array.size()-1 && array[right] > array[largest]){
 		largest = right;
 	}
 	
 	if (largest != index){
-		int temp = array[index];
-		array[index] = array[largest];
-		array[largest] = temp;
-		
+		swap(array[index], array[largest]);
 		heapify(array, largest);
 	}
-
-	
 }
